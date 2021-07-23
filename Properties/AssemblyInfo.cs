fd3b4d0144c1d29ef955a5ecbfc5c1ee735cf0fe
rjtwins <jorre_vedder@hotmail.com>
2021-07-23 11:28:18 +0200

    updated AssemblyInfo.cs
    ci skip

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,13 +5,13 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MW5 Mod Manager")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("MW5 Load Order and Activation Manager")]
+[assembly: AssemblyDescription("A load order and activation manager for Mechwarrior 5")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MW5 Mod Manager")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCompany("RJ Not a Company")]
+[assembly: AssemblyProduct("MW5 LOM")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("NA")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("c04195f2-ec6b-4280-a0aa-8699c513dd5c")]
+[assembly: Guid("3aff4086-6f6d-457d-adfa-d442f36963c7")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.1.0.0")]
+[assembly: AssemblyFileVersion("2.1.0.0")]
